75d221bb4d6de62106bd78d3ce67573a0f737349
Oxters <oxters168@gmail.com>
2024-09-07 00:34:35 -0400

    Prepared Odin 2 Mini picture. Got the toolbar to have a sidenav with buttons and capable of having a child view.

--- src/app/toolbar/toolbar.component.ts
+++ src/app/toolbar/toolbar.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core'
+import { Component, ViewChild } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { MatIconModule } from '@angular/material/icon'
 import { MatDividerModule } from '@angular/material/divider'
 import { MatButtonModule } from '@angular/material/button'
 import { MatToolbarModule } from '@angular/material/toolbar'
 import { MatTooltipModule } from '@angular/material/tooltip'
+import { MatDrawer, MatSidenavModule } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-toolbar',
@@ -15,13 +16,17 @@ import { MatTooltipModule } from '@angular/material/tooltip'
     MatIconModule,
     MatDividerModule,
     MatButtonModule,
-    MatTooltipModule
+    MatTooltipModule,
+		MatSidenavModule
   ],
   templateUrl: './toolbar.component.html',
   styleUrl: './toolbar.component.scss'
 })
+
 export class ToolbarComponent {
   isDarkMode = false
+	@ViewChild('drawer')
+	drawerRef!: MatDrawer
 
   constructor() {
     const val = localStorage.getItem('isDarkMode')
@@ -30,6 +35,10 @@ export class ToolbarComponent {
     }
   }
   
+	goto(page: Page) {
+		console.log(page)
+		this.drawerRef.close()
+	}
   toggleDarkMode() {
     this.setDarkMode(!this.isDarkMode)
   }
@@ -43,3 +52,5 @@ export class ToolbarComponent {
     }
   }
 }
+
+export type Page = 'ox-shell' | 'ox-board' | 'little-trinkets' | 'contact'
